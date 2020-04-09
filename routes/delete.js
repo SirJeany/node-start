d2fa508dfc35077a040105f548f4f425a1ed7304
SirJeany <jmvanriet1@gmail.com>
2020-04-09 12:56:38 +0200

    Tryin to get the routes linked in app.js

--- routes/delete.js
+++ routes/delete.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.get('/:id', function(req, res, next){
+router.get('/:pokeId', function(req, res, next){
     request.get(
         'http://localhost:8000/pokemon/' + req.body.pokeId,
         function(error, response, body){

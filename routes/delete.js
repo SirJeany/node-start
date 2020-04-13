47b107f4b6d84c237c9e04f929a95002c2825d3d
SirJeany <jmvanriet1@gmail.com>
2020-04-13 11:47:00 +0200

    Changed little, removed most useless comments and added some useful ones.

--- routes/delete.js
+++ routes/delete.js
@@ -5,43 +5,28 @@ const request = require('request'); // Request to handle requests
 
 // Register the router with the app:
 router.get('/:pokeId', function(req, res, next){
-
+  console.log("GET DELETE");
     // NATE:
-    var id = 0;
+    var deletedIndex = 0; // The index which the poke is to be deleted at
     var Pokes = Pokemon.pokemon;
-    console.log(req.params.pokeId);
     // loop to find index
     for(var i = 0; i < Pokes.length; i++){
       if (Pokes[i].id == req.params.pokeId){
-        id = i;
+        deletedIndex = i;
       }
     }
 
-    
+    console.log("deletedIndex = " + deletedIndex)
+    console.log("req.params.pokeId = " + req.params.pokeId)
 
+    // Instead of passing the body, like in views, we pass the db.json object. 
+    // We then use the deletedIndex as a reference to the index in the object array  and not req.params.pokeId, which would be one ahead.
     res.render('delete',{
       title : "Delete",
       poke : Pokemon.pokemon,
-      id : id,
+      index : deletedIndex,
     });
 
-    // Find id in the db:
-    // var pokes = Pokemon.pokemon;
-    // var id = Number(pokes.length);
-
-    // var pokesId = pokes[1].id;
-
-    // for(var i = 0; i <= pokes.length; i++){
-    //     if(pokesId == id){
-    //         id = i;
-    //     }
-    // }
-    // console.log(id)
-    // res.render('delete', {
-    //     title: "Delete",
-    //     poke: Pokemon.pokemon[req.params.pokeId],
-    //     id: id
-    // });
 });
 
 router.post('/:pokeId', function(req, res, next){
